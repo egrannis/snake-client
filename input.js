330532975db30aa4702b9f698222c10d184f02b6
Emma Grannis <grannis.emma@gmail.com>
2022-04-13 22:54:35 +0000

    Got my snake to move with keyboard keys and finished all steps. Going to revisit to lint check and clean up text

--- input.js
+++ input.js
@@ -1,20 +1,39 @@
-const handleUserInput = function (key) {//does this go inside the setupinput function??
+let connection;
+
+const handleUserInput = function (key, connection) {//does this go inside the setupinput function??
   if (key === '\u0003') {
     process.exit();
     }
+  if (key === 'w') {
+    connection.write('Move: up')
+  }
+  if (key === 'a') {
+    connection.write('Move: left')
+    }
+  if (key === 's' ) {
+    connection.write('Move: down')
+    }
+  if (key === 'd') {
+    connection.write('Move: right')
+    }
   }
   
-  const setupInput = function () {
+  // setTimeout(() => conn.write('Move: up'), 500);
+    // setTimeout(() => conn.write('Move: right'), 550);
+  
+  const setupInput = function (conn) {
+    connection = conn;
     const stdin = process.stdin;
     stdin.setRawMode(true);
     stdin.setEncoding("utf8");
     stdin.resume();
-    stdin.on("data", handleUserInput);
+    stdin.on("data", (data) => { 
+      handleUserInput(data, connection)} );
     return stdin;
   };
 
   module.exports = { setupInput };
-  
+
   //handleUserInput is only called by setupInput which is already in the same file. 
   // Since handleUserInput does not need to be called or referenced elsewhere, 
   // it does not need to be exported from the input module.
